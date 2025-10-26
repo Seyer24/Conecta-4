cdc533763e480bbb257ddbdd567f5d6673b300a5
Arath Reyes <arath.reyes@uabc.edu.mx>
2025-10-26 06:48:21 +0000

    Meti Librerias, inicie prototipos y menu
    Ya meti un ciclo do while, yo creo que estara iniciar con un menu pero con do para que se inicie obligatorio, para poder decidir si iniciar ahora si bien el juego o no iniciarlo.
    Ahorita hago el prototipo de iniciar el juego, donde pedire los nombres de los jugadores

--- Conecta4.c
+++ Conecta4.c
@@ -1,4 +1,18 @@
+/*
+
+-------------Proyecto Conecta 4------------------
+Equipo: Nadia y Arath Reyes Gonzalez
+Universidad Autonoma de Baja California
+Lenguaje C
+
+*/
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void IniciarJuego();
+void AcabarJuego();
 
 //Defininos el tamano del tablero 
 #define COLUMNAS 6
@@ -6,8 +20,20 @@
 
 int main(){
 
-  printf("\nHola\n");
-  printf("Hola");
+  int opc=0;
+  
+  do{
+    
+    printf("\nBienvenido al proyecto Conecta 4\n");
+    printf("\nIniciar el juego? 1=si ");
+    printf("\nSalir del juego?  2=nO ");
+    scanf("%d", &opc);
 
+  } while (opc = 1)
+      printf("\nDesea volver al menu? Si=1 Y terminar el juego=2  ");
+        scanf("%d", &opc);
+  
 return 0;
 }
+
+
